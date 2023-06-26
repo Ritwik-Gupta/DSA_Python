2069443010dd22043d6dc3bc3b2860759dff18f7
Ritwik <ritwik94348gupta@gmail.com>
2023-06-27 02:02:09 +0530

    first commit

---
+++ Python - Udemy/Linked Lists/LinkedList.py
@@ -0,0 +1,90 @@
+#Creates a node
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None    
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = Node(value)
+        self.head =  self.value
+        self.tail = self.value
+        self.length = 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if(self.head == None):
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.length += 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if(self.head == None):
+            self.head =  new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length += 1
+
+    def pop(self):
+        if(self.head is not None):
+            curr_node = self.head
+            if(curr_node.next is None): #LL with only 1 node
+                self.head = None
+                self.tail = None
+            else:
+                while(curr_node.next.next is not None): #getting the 2nd last node
+                    curr_node = curr_node.next
+
+                self.tail = curr_node
+                curr_node.next = None
+            
+            self.length -= 1
+    
+
+    def print(self):
+        curr_node = self.head
+        while(curr_node != None):
+            print(curr_node.value, end=" ")
+            curr_node = curr_node.next
+
+
+l1 = LinkedList(3)
+
+l1.append(2)
+l1.append(5)
+
+l1.print()
+print()
+
+l1.prepend(11)
+l1.prepend(12)
+
+l1.print()
+print()
+
+l1.pop()
+l1.pop()
+l1.pop()
+l1.pop()
+
+l1.print()
+print()
+
+l1.append(8)
+l1.append(10)
+
+l1.print()
+print()
+
+l1.pop()
+l1.pop()
+l1.print()
+print()
