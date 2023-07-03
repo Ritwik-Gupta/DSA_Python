266ad802afdae2e0a10720cfac844ba5a5fe8fb8
Ritwik <ritwik94348gupta@gmail.com>
2023-07-03 22:26:57 +0530

    Coding exercises for doubly linked list added

--- Python - Udemy/Linked Lists/Doubly Linked Lists/DoublyLinkedList.py
+++ Python - Udemy/Linked Lists/Doubly Linked Lists/DoublyLinkedList.py
@@ -73,8 +73,13 @@ class DoublyLinkedList:
             if(idx == self.length-1):
                 return self.tail
             curr_node = self.head
-            for _ in range(idx):
-                curr_node = curr_node.next
+            if(idx < int(self.length/2)):
+                for _ in range(idx):
+                    curr_node = curr_node.next
+            else:
+                curr_node = self.tail
+                for _ in range(self.length-1, idx, -1):
+                    curr_node = curr_node.prev
             return curr_node
         
     def set(self, idx, value):
@@ -84,6 +89,42 @@ class DoublyLinkedList:
             curr_node = self.get(idx)
             curr_node.value = value
             return True
+        
+    def insert(self, idx, value):
+        if(idx < 0 or idx > self.length):
+            return False
+        else:
+            if(idx == 0):
+                self.prepend(value)
+                return True
+            if(idx == self.length):
+                self.append(value)
+                return True
+            new_node = Node(value)
+            prev_node = self.get(idx-1)
+            new_node.next = prev_node.next
+            prev_node.next.prev   = new_node
+            prev_node.next = new_node
+            new_node.prev = prev_node
+            self.length += 1
+            return True
+        
+    def remove(self, idx):
+        if(idx < 0 or idx >= self.length):
+            return None
+        else:
+            if(idx == 0):
+                return self.pop_first()
+            if(idx == self.length-1):
+                return self.pop()
+            curr_node = self.get(idx)
+            curr_node.prev.next = curr_node.next
+            curr_node.next.prev = curr_node.prev
+            curr_node.next = None
+            curr_node.prev = None
+            self.length -= 1
+            return curr_node
+
 
 
     def print(self):
@@ -95,6 +136,7 @@ class DoublyLinkedList:
 
 
 l1 = DoublyLinkedList(0)
+
 
 l1.append(1)
 l1.append(2)
@@ -108,6 +150,19 @@ l1.pop_first()
 l1.pop_first()
 l1.print()
 
-l1.set(4,44)
+l1.set(3,69)
 l1.print()
 
+l1.insert(3,9)
+l1.print()
+
+l1.insert(0,99)
+l1.print()
+l1.insert(6 ,99)
+l1.print()
+
+l1.remove(2)
+l1.print()
+
+l1.remove(3)
+l1.print()
