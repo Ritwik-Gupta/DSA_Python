0f6eaa432194c0cf01db0cfe70beebb411679c5c
Ritwik <ritwik94348gupta@gmail.com>
2023-07-03 23:53:08 +0530

    after some fixes

--- Python - Udemy/Stacks and Queues/Queue.py
+++ Python - Udemy/Stacks and Queues/Queue.py
@@ -1,7 +1,8 @@
 class Node:
-    def  __init__(self, value):
+    def __init__(self, value):
         self.value = value
         self.next = None
+
 
 class Queue:
     def __init__(self, value):
@@ -12,32 +13,29 @@ class Queue:
 
     def enqueue(self, value):
         new_node = Node(value)
-        if(self.length == 0):
+        if (self.length == 0):
             self.first = new_node
             self.last = new_node
-            self.length = 1
         else:
-            new_node.next = self.first
-            self.first = new_node
+            self.last.next = new_node
+            self.last = new_node
         self.length += 1
         return True
 
     def dequeue(self):
-        if(self.length == 0):
+        if (self.length == 0):
             return None
-        temp = self.last
-        if(self.length == 1):
+        temp = self.first
+        if (self.length == 1):
             self.first = None
             self.last = None
+            self.length -= 1
             return temp
-        curr_node = self.first
-        while curr_node.next.next:
-            curr_node = curr_node.next
-        curr_node.next = None
-        self.last = curr_node
+        self.first = self.first.next
         self.length -= 1
+        temp.next = None
         return temp
-    
+
     def print(self):
         curr_node = self.first
         while curr_node:
@@ -45,7 +43,7 @@ class Queue:
             curr_node = curr_node.next
         print()
 
-        
+
 q1 = Queue(1)
 
 q1.enqueue(2)
@@ -58,3 +56,6 @@ q1.dequeue()
 q1.dequeue()
 q1.print()
 
+q1.enqueue(6)
+q1.enqueue(7)
+q1.print()
